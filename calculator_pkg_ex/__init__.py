72aae1b4efd47c0d1684aaacd1c00c8540b97d57
OtikD <otikd2@gmail.com>
2026-07-03 15:18:20 +0200

    Adding tests

--- calculator_pkg_ex/__init__.py
+++ calculator_pkg_ex/__init__.py
@@ -1,3 +1,6 @@
+# from .file_calculator import FileCalculator
+# from . import file_calculator
 from .calculator import Calculator
+from .file_calculator import FileCalculator
 
 __all__ = ["Calculator"]
--- calculator_pkg_ex/file_calculator/file_calculator.py
+++ calculator_pkg_ex/file_calculator/file_calculator.py
@@ -1,6 +1,21 @@
+from pathlib import Path
+
 from ..calculator import Calculator
+
+# from calculator_pkg_ex.calculator import Calculator
 
 
 class FileCalculator(Calculator):
-    def __init__(self):
-        pass
+    def __init__(self, path=Path(__file__).parent / "nums.csv"):
+        self.path = path
+
+    def add_file(self):
+        total = None
+        with open(self.path, "r") as f:
+            for line in f:
+                if total is None:
+                    total = line
+                    continue
+                else:
+                    total += line
+        return total
